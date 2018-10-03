c7e5c11c372c2f8084f1b3702c4291cc909efee3
SheikhMohsinSaleem <msaleem2017@namal.edu.pk>
2018-10-03 17:38:40 +0500

    just started

---
+++ task3.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+class Invoice{
+public:
+
+
+
+
+
+
+private:
+  string part_number , part_description;
+  int item_quantity;
+  double item_price;
+
+};
+
+int main() {
+  
+  return 0;
+}
